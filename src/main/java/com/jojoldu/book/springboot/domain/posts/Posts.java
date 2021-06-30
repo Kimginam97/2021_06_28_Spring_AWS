663b566b8def690b9ad07f55e29f5dc93afdff3a
Leewoowon980522 <hjww0522@gmail.com>
2021-06-30 14:29:16 +0900

    Spring data Jpa적용

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+// 실제 DB와 매칭될 클래스(Entity 클래스)
+@Getter
+@NoArgsConstructor//기본 생성자 자동추가
+@Entity// 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+    // 해당 테이블의 PK필드를 나타냄
+    @Id
+    // PK의 생성 규칙을 나타내며 스프링부트2.0에서는 GenerationType.IDENTITY 옵션을 추가해야 auto_increment가 됨
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //테이블의 칼럼을 나타내며 선언하지 않아도 됨
+    @Column(length = 500, nullable = false)//길이가 500,타입은 TEXT
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder//해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After//단위 테스트가 끝날 때마다 수행되는 메소드를 지정
+    public void cleanup(){
+        postsRepository.deleteAll();//데이터 전체 삭제
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()//save는 insert,update쿼릴르 실행
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();//모든 데이터를 조회
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
